7ac2c11eb2e89b8946bd72d36f953cf83d48334f
Islambek123 <topkokos1337@gmail.com>
2017-12-20 17:22:27 +0200

    git rofl

--- kursov/kursov/MainWindow.xaml.cs
+++ kursov/kursov/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using kursov.Context;
 
 namespace kursov
 {
@@ -20,9 +22,54 @@ namespace kursov
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static EFContext _efContext;
+        private readonly BackgroundWorker backgroundWorker;
+
+        private string role;
+        public string Role { set { role = value; } }
         public MainWindow()
         {
+            backgroundWorker = new BackgroundWorker();
+            backgroundWorker.DoWork += DoConnect;
+            backgroundWorker.WorkerSupportsCancellation = true;
             InitializeComponent();
+        }
+
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            if (!backgroundWorker.IsBusy)
+                backgroundWorker.RunWorkerAsync();
+            else
+                backgroundWorker.CancelAsync();
+
+        }
+
+        private void DoConnect(object sender, DoWorkEventArgs e)
+        {
+            foreach (var item in _efContext.login.ToList())
+            {
+                if (txtEmail.Text == item.Email && txtPassword.Text == item.Password)
+                {
+                    var role = item.role.FirstOrDefault(r => r.RoleName == "Admin");
+
+                    if (role != null)
+                    {
+                        Role = role.RoleName;
+                    }
+                    else
+                    {
+                        Role = string.Empty;
+                    }
+                    break;
+                }
+            }
+        }
+
+
+        private void MainForm_Loaded(object sender, RoutedEventArgs e)
+        {
+            _efContext = new EFContext();
         }
     }
 }
